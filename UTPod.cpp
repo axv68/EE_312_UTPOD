f6b320b064d2f91c4a4bc693bb0e64ce95d94dda
Amit verma <amitverma@wireless-10-145-3-190.public.utexas.edu>
2019-10-23 11:33:43 -0500

    listed all methods in UTPod.cpp from header file

--- UTPod.cpp
+++ UTPod.cpp
@@ -15,4 +15,29 @@ int UtPod::addSong(Song const &s){
 
 }
 
+int UtPod::removeSong(Song const &s){ 
+
+}
+
+void UtPod::shuffle(){ 
+
+
+}
+
+void UtPod::sortSongList(){ 
+
+}
+
+void UtPod::clearMemory(){ 
+
+}
+
+int UtPod::getRemainingMemory(){ 
+
+}
+
+UtPod::~UtPod(){
+
+}
+
 
