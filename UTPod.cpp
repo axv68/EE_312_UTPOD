f2b7dbe3e5f00d8fef030577ad1ff9d59c96edd0
Amit verma <amitverma@wireless-10-145-3-190.public.utexas.edu>
2019-10-23 11:48:17 -0500

    Finished a few UTPod methods

--- UTPod.cpp
+++ UTPod.cpp
@@ -4,11 +4,16 @@
 using namespace std; 
 
 UtPod::UtPod(){ 
-
+    memSize = MAX_MEMORY; 
 }
 
 UtPod::UtPod(int size){ 
-
+    if ((size > MAX_MEMORY) || (size <= 0)){
+        memSize = MAX_MEMORY; 
+    }
+    else {
+        memSize = size; 
+    }
 }
 
 int UtPod::addSong(Song const &s){ 
@@ -32,8 +37,14 @@ void UtPod::clearMemory(){
 
 }
 
-int UtPod::getRemainingMemory(){ 
-
+int UtPod::getRemainingMemory(SongNode *songs){  //Amit Changed the input parameter for this function
+    int currentTotal = 0; 
+    while (songs != NULL){ 
+        currentTotal = currentTotal + songs->s.getSize; 
+        songs = songs->next; 
+    }
+    int remainingMem = memSize - currentTotal; 
+    return (currentTotal); 
 }
 
 UtPod::~UtPod(){
